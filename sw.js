94d6680648a20de3cf8bd516e9bf10f7b1e45bf3
zheng-de-hui <2199953529@qq.com>
2026-09-10 21:23:23 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,41 @@
-const CACHE='scnu-pusheng-v64-slim';
+const CACHE='scnu-pusheng-v64-objective-final';
 const ASSETS=['./','./index.html','./data.js','./terms.js','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(
-  caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())
-));
-self.addEventListener('activate',e=>e.waitUntil(
-  caches.keys().then(keys=>Promise.all(
-    keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
-  )).then(()=>self.clients.claim())
-));
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
-  e.respondWith(
-    caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{
-      const copy=r.clone();
-      caches.open(CACHE).then(x=>x.put(e.request,copy));
-      return r;
-    }).catch(()=>caches.match('./index.html')))
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+
+      return fetch(event.request)
+        .then(response => {
+          if (!response || response.status !== 200 || response.type === 'opaque') {
+            return response;
+          }
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'));
+    })
   );
 });
